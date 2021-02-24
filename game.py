7f52ec33da4e1c335fdc7eed7c7f4b6981ec19b0
Kelsi <kelsi.flatland@gmail.com>
2021-02-24 00:27:59 +0000

    initial commit
    Co-authored by: Tina Haiser <tinahaiser@gmail.com>

---
+++ game.py
@@ -0,0 +1,13 @@
+"""A number-guessing game."""
+# Put your code here
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
